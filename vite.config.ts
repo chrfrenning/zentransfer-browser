cb354d21b4e8f0a377fd6aabfafa491abe76f7e9
Christopher Frenning <christopher@frenning.com>
2025-07-21 01:57:21 +0200

    Initial scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ['localhost', '127.0.0.1', 'tmp.chph.dev']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
+      }
+    }
   }
 })
